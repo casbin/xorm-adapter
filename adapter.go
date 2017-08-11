6d28ef80f4e64d865081a63145757101326af8fd
Yang Luo <hsluoyz@qq.com>
2017-08-11 14:15:51 +0800

    Add SQL index to gain speed.

--- adapter.go
+++ adapter.go
@@ -25,13 +25,13 @@ import (
 )
 
 type CasbinRule struct {
-	PType string `xorm:"varchar(100)"`
-	V0    string `xorm:"varchar(100)"`
-	V1    string `xorm:"varchar(100)"`
-	V2    string `xorm:"varchar(100)"`
-	V3    string `xorm:"varchar(100)"`
-	V4    string `xorm:"varchar(100)"`
-	V5    string `xorm:"varchar(100)"`
+	PType string `xorm:"varchar(100) index"`
+	V0    string `xorm:"varchar(100) index"`
+	V1    string `xorm:"varchar(100) index"`
+	V2    string `xorm:"varchar(100) index"`
+	V3    string `xorm:"varchar(100) index"`
+	V4    string `xorm:"varchar(100) index"`
+	V5    string `xorm:"varchar(100) index"`
 }
 
 // Adapter represents the Xorm adapter for policy storage.
